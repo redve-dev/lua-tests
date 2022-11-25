3536eb830c49c2db9ad61d91245fba55e5553458
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-25 04:47:52 +0100

    add more functions to test

--- src/main.c
+++ src/main.c
@@ -13,13 +13,25 @@ int call_lua_add(lua_State* L, int a, int b){
 	return sum;
 }
 
+double call_lua_sqrt(lua_State* L, double n, double e){
+	lua_getglobal(L, "sqrt");
+	lua_pushnumber(L, n);
+	lua_pushnumber(L, e);
+	lua_call(L, 2, 1);
+	double sum = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+	return sum;
+
+}
+
 int main(){
 	lua_State* L;
 	L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dofile(L, "test.lua");
-	int sum = call_lua_add(L, 5, 3);
-	printf("%d\n", sum);
+	/*int sum = call_lua_add(L, 5, 3);*/
+	double sum = call_lua_sqrt(L, 5, 0.001);
+	printf("%lf\n", sum);
 	lua_close(L);
 	return 0;
 }
--- src/test.lua
+++ src/test.lua
@@ -1,3 +1,15 @@
 add = function(a, b)
 	return a+b
 end
+
+-- Newton-Rawson algorithm
+sqrt = function(n, epsilon)
+	local a=n
+	local b=1
+	while ( math.abs(a-b) >= epsilon )
+		do
+			a=(a+b)/2
+			b=n/a
+		end
+	return a
+end
