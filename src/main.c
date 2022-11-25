feeb55d0991ba6c42edd4ed501fbe061f50c9669
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-25 05:23:30 +0100

    add request caller

--- src/main.c
+++ src/main.c
@@ -24,14 +24,27 @@ double call_lua_sqrt(lua_State* L, double n, double e){
 
 }
 
+void call_lua_http_request(lua_State* L, const char* api){
+	lua_getglobal(L, "make_request");
+	lua_pushstring(L, api);
+	lua_call(L, 1, 1);
+	const char* response = lua_tostring(L, -1);
+	lua_pop(L, 1);
+	lua_getglobal(L, "print_json");
+	lua_pushstring(L, response);
+	lua_call(L, 1, 1);
+}
+
 int main(){
 	lua_State* L;
 	L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dofile(L, "test.lua");
 	/*int sum = call_lua_add(L, 5, 3);*/
-	double sum = call_lua_sqrt(L, 5, 0.001);
-	printf("%lf\n", sum);
+	/*double sum = call_lua_sqrt(L, 5, 0.001);*/
+	/*printf("%lf\n", sum);*/
+	const char* api="https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=Fracture%20Case";
+	call_lua_http_request(L, api);
 	lua_close(L);
 	return 0;
 }
--- src/test.lua
+++ src/test.lua
@@ -1,15 +1,22 @@
-add = function(a, b)
-	return a+b
+make_request = function(api)
+	local http_request = require "http.request"
+	local headers, stream = assert(http_request.new_from_uri(api):go())
+	local body = assert(stream:get_body_as_string())
+	if headers:get ":status" ~= "200" then
+		error(body)
+	end
+	return body
 end
 
--- Newton-Rawson algorithm
-sqrt = function(n, epsilon)
-	local a=n
-	local b=1
-	while ( math.abs(a-b) >= epsilon )
+local api="https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=Fracture%20Case"
+local result = make_request(api)
+
+print_json = function(data)
+	local json_lib = require "lunajson"
+	local arr=json_lib.decode(data)
+
+	for key, value in pairs(arr)
 		do
-			a=(a+b)/2
-			b=n/a
+			print(key, value)
 		end
-	return a
 end
